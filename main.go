e6bf042b1e7327e5987f6cfbd074f2b9b865dfb5
mac <mac@macbookdeMBP.lan>
2022-03-25 23:23:00 +0800

    init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("algorithm tool by jiftle 2022")
+
+	w.Resize(fyne.NewSize(600, 400))
+	w.CenterOnScreen()
+	gui := newGUI()
+
+	w.SetContent(gui.makeUI())
+	w.ShowAndRun()
+}
