16b2701133821a69a0342774071cf7d80de70b33
john <john@work.com>
2022-03-28 18:21:36 +0800

    update readme

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"os"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 )
 
 var w fyne.Window
+
+func init() {
+	// linux 指定支持中文的字体
+	// os.Setenv("FYNE_FONT", "/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf")
+	os.Setenv("FYNE_THEME", "light")
+}
 
 func main() {
 	a := app.New()
